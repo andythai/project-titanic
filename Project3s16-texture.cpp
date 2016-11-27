296313bd62e9203539f783e47ddbf2c6c94928fe
Andy Thai <andy.thai9@gmail.com>
2016-11-27 14:06:28 -0800

    Skybox code
    Classes for skybox and shaders

---
+++ Project3s16-texture.cpp
@@ -0,0 +1,92 @@
+#define _CRT_SECURE_NO_DEPRECATE
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>   // include math functions, such as sin, cos, M_PI
+#include <iostream> // allow c++ style console printouts
+
+using namespace std; // allow console printouts without std::
+
+/** Load a ppm file from disk.
+ @input filename The location of the PPM file.  If the file is not found, an error message
+		will be printed and this function will return 0
+ @input width This will be modified to contain the width of the loaded image, or 0 if file not found
+ @input height This will be modified to contain the height of the loaded image, or 0 if file not found
+ @return Returns the RGB pixel data as interleaved unsigned chars (R0 G0 B0 R1 G1 B1 R2 G2 B2 .... etc) or 0 if an error ocured
+**/
+unsigned char* loadPPM(const char* filename, int& width, int& height)
+{
+	const int BUFSIZE = 128;
+	FILE* fp;
+	unsigned int read;
+	unsigned char* rawData;
+	char buf[3][BUFSIZE];
+	char* retval_fgets;
+	size_t retval_sscanf;
+
+	if ( (fp=fopen(filename, "rb")) == NULL)
+	{
+		std::cerr << "error reading ppm file, could not locate " << filename << std::endl;
+		width = 0;
+		height = 0;
+		return NULL;
+	}
+
+	// Read magic number:
+	retval_fgets = fgets(buf[0], BUFSIZE, fp);
+
+	// Read width and height:
+	do
+	{
+		retval_fgets=fgets(buf[0], BUFSIZE, fp);
+	} while (buf[0][0] == '#');
+	retval_sscanf=sscanf(buf[0], "%s %s", buf[1], buf[2]);
+	width  = atoi(buf[1]);
+	height = atoi(buf[2]);
+
+	// Read maxval:
+	do
+	{
+	  retval_fgets=fgets(buf[0], BUFSIZE, fp);
+	} while (buf[0][0] == '#');
+
+	// Read image data:
+	rawData = new unsigned char[width * height * 3];
+	read = fread(rawData, width * height * 3, 1, fp);
+	fclose(fp);
+	if (read != 1)
+	{
+		std::cerr << "error parsing ppm file, incomplete data" << std::endl;
+		delete[] rawData;
+		width = 0;
+		height = 0;
+		return NULL;
+	}
+
+	return rawData;
+}
+
+// load image file into texture object
+void loadTexture()
+{
+  GLuint texture[1];     // storage for one texture
+  int twidth, theight;   // texture width/height [pixels]
+  unsigned char* tdata;  // texture pixel data
+  
+  // Load image file
+  tdata = loadPPM("image.ppm", twidth, theight);
+  if (tdata==NULL) return;
+  
+  // Create ID for texture
+  glGenTextures(1, &texture[0]);   
+
+  // Set this texture to be the one we are working with
+  glBindTexture(GL_TEXTURE_2D, texture[0]);
+  
+  // Generate the texture
+  glTexImage2D(GL_TEXTURE_2D, 0, 3, twidth, theight, 0, GL_RGB, GL_UNSIGNED_BYTE, tdata);
+  
+  // Set bi-linear filtering for both minification and magnification
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR); 
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+}
+
---
+++ Skybox.h
@@ -0,0 +1,65 @@
+#ifndef _SKYBOX_H_
+#define _SKYBOX_H_
+
+#define GLFW_INCLUDE_GLEXT
+#ifdef __APPLE__
+#define GLFW_INCLUDE_GLCOREARB
+#else
+#include <GL/glew.h>
+#endif
+#include <GLFW/glfw3.h>
+// Use of degrees is deprecated. Use radians instead.
+#ifndef GLM_FORCE_RADIANS
+#define GLM_FORCE_RADIANS
+#endif
+#include <glm/mat4x4.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+class Skybox
+{
+public:
+	Skybox();
+	Skybox(float mult);
+	~Skybox();
+
+	glm::mat4 toWorld;
+
+	void initialize();
+	void draw(GLuint);
+	void sendLight(GLuint shaderProgram);
+
+	// Cubemap
+	GLuint loadCubemap();
+	GLuint textureID;
+
+	// These variables are needed for the shader program
+	GLuint VBO, VAO, EBO;
+	GLuint uProjection, uModelview, uModel, uView;
+
+	GLfloat vertices[8][3] = {
+		// "Front" vertices
+		{ -2.0, -2.0,  2.0 },{ 2.0, -2.0,  2.0 },{ 2.0,  2.0,  2.0 },{ -2.0,  2.0,  2.0 },
+		// "Back" vertices
+		{ -2.0, -2.0, -2.0 },{ 2.0, -2.0, -2.0 },{ 2.0,  2.0, -2.0 },{ -2.0,  2.0, -2.0 }
+	};
+};
+
+// Note that GL_QUADS is deprecated in modern OpenGL (and removed from OSX systems).
+// This is why we need to draw each face as 2 triangles instead of 1 quadrilateral
+const GLuint indices[6][6] = {
+	// Front face
+	{0, 1, 2, 2, 3, 0},
+	// Top face
+	{1, 5, 6, 6, 2, 1},
+	// Back face
+	{7, 6, 5, 5, 4, 7},
+	// Bottom face
+	{4, 0, 3, 3, 7, 4},
+	// Left face
+	{4, 5, 1, 1, 0, 4},
+	// Right face
+	{3, 2, 6, 6, 7, 3}
+};
+
+#endif
+
