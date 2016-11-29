a6d27d273a230a2c51c7dd2a0a2ded4e00a3fdf6
Tommy Yang <yt940928@gmail.com>
2016-11-28 16:50:55 -0800

    add Audio class

---
+++ Audio.h
@@ -0,0 +1,68 @@
+//
+//  Audio.h
+//  CSE167Final
+//
+
+#ifndef Audio_hpp
+#define Audio_hpp
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include <unistd.h>
+#include <stdbool.h>
+
+#ifdef __APPLE__
+#include <OpenAL/al.h>
+#include <OpenAL/alc.h>
+#else
+#include <AL/al.h>
+#include <AL/alc.h>
+#endif
+
+#include <iostream>
+#include <string>
+#include <fstream>
+
+#define TEST_ERROR(_msg)		\
+error = alGetError();		\
+if (error != AL_NO_ERROR) {	\
+fprintf(stderr, _msg "\n");	\
+return;		\
+}
+
+class Audio {
+public:
+    ALCdevice* device;
+    ALCcontext* context;
+    ALenum format;
+    ALuint buffer;
+    ALuint source;
+    ALCenum error;
+    ALint source_state;
+
+    Audio(std::string filename);
+    ~Audio();
+    void play();
+
+    static bool checkBigEndian() {
+        int i = 1;
+        return !((char*)&i)[0];
+    }
+
+    static int convertToInt(char* buffer, int len) {
+        int a = 0;
+        if (!checkBigEndian())
+            for (int i = 0; i < len; i++)
+                ((char*)&a)[i] = buffer[i];
+        else
+            for (int i = 0; i < len; i++)
+                ((char*)&a)[3 - i] = buffer[i];
+        return a;
+    }
+};
+
+#endif /* Audio_hpp */
